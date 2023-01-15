3d6f7cd261d86c9e7a7c65fcd0936d3cac9ff6a4
gusmendesbh <101278278@georgebrown.ca>
2023-01-15 10:19:49 -0500

    Finalized file, some calculation erros that Ill fix if i have time

--- PyBank/Analysis/FinancialAnalysis.py
+++ PyBank/Analysis/FinancialAnalysis.py
@@ -3,13 +3,18 @@ import os
 import csv
 
 # declaring necessary variables
-rowNumber = 0
+rowNumber = []
+rowName = []
+rowsList = []
+increase = []
+totalRows = 0
+totalIncrease = []
 rowTotal = 0
-averageCharge = 0
+averageChange = 0
 greatestIncrease = 0
-increaseMonth = " "
+increaseMonth = ""
 greatestDecrease = 0
-decreaseMonth = " "
+decreaseMonth = ""
 
 # setting the cross platform csv path
 csvPath = os.path.join("..", "Resources", "budget_data.csv")
@@ -23,35 +28,45 @@ with open(csvPath, 'r') as csvFile:
 
     # looping throught the file to read and count data
     for row in csvReader:
-        rowNumber += 1
         rowTotal = rowTotal + int(row[1])
-        averageCharge = rowTotal / rowNumber  # incorrect
-        if int(row[1]) > int(greatestIncrease):
-            increaseMonth = row[0]
-            greatestIncrease = row[1]
+        rowsList.append(row)
+        rowName.append(row[0])
+        rowNumber.append(row[1])
+totalRows = len(rowNumber)
 
-        if int(row[1]) < int(greatestDecrease):
-            decreaseMonth = row[0]
-            greatestDecrease = row[1]
+# calculating total, increase, decrease and average change
+for i in range(len(rowsList)):
+    increase = int(rowNumber[i]) - int(rowNumber[i-1])
+    totalIncrease.append(increase)
+greatestDecrease = min(totalIncrease)
+greatestIncrease = max(totalIncrease)
+averageChange = (sum(totalIncrease)/totalRows)
 
-    # printing results on terminal
-    print('Financial Analysis')
-    print('-------------------------')
-    print('Total Months: ' + str(rowNumber))
-    print('Total: $' + str(rowTotal))
-    print(f'Average Change: $ {averageCharge:.2f}')
-    print('Greatest Increase in Profits: ' +
-          increaseMonth + '($' + str(greatestIncrease) + ")")
-    print('Greatest Decrease in Profits: ' +
-          decreaseMonth + '($' + str(greatestDecrease) + ")")
+# parsing the month name
+for i in range(len(rowsList)):
+    if int(rowsList[i][1]) == greatestDecrease:
+        decreaseMonth = rowsList[i]
+    elif int(rowsList[i][1]) == greatestIncrease:
+        increaseMonth = rowsList[i]
+
+# printing results on terminal
+print('Financial Analysis')
+print('-------------------------')
+print(f'Total Months: {totalRows}')
+print('Total: $' + str(rowTotal))
+print(f'Average Change: $ {averageChange:.2f}')
+print('Greatest Increase in Profits: ' +
+      increaseMonth + '($' + str(greatestIncrease) + ")")
+print('Greatest Decrease in Profits: ' +
+      decreaseMonth + '($' + str(greatestDecrease) + ")")
 
 # printing results on txt file
 with open("analysis.txt", 'w') as txtFile:
     print('Financial Analysis', file=txtFile)
     print('-------------------------', file=txtFile)
-    print('Total Months: ' + str(rowNumber), file=txtFile)
+    print(f'Total Months: {totalRows}', file=txtFile)
     print('Total: $' + str(rowTotal), file=txtFile)
-    print(f'Average Change: $ {averageCharge:.2f}', file=txtFile)
+    print(f'Average Change: $ {averageChange:.2f}', file=txtFile)
     print('Greatest Increase in Profits: ' +
           increaseMonth + '($' + str(greatestIncrease) + ")", file=txtFile)
     print('Greatest Decrease in Profits: ' +
